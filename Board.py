550d6f79c272b88c837b151626f70613a6a9f53e
BuildTools <unconfigured@null.spigotmc.org>
2017-09-29 13:23:56 +0200

    Added Capture management, and simplified board representation

--- Board.py
+++ Board.py
@@ -1,4 +1,6 @@
 import Piece as p
+import Capture as c
+import Piece as p
 
 
 class Board:
@@ -8,35 +10,37 @@ class Board:
         board = []
         for row in range(size):
             board.append([])
-            for col in range(size//2):
+            for col in range(size):
                 case = ""
-                if row < size//2 - 1:
-                    case = p.Piece("N", (row, col))
-                elif row > size//2:
-                    case = p.Piece("B", (row, col))
+                if (col % 2 == 0 and row % 2 != 0) \
+                    or (col % 2 != 0 and row % 2 == 0):
+
+                    if row < size//2 - 1:
+                        case = p.Piece("N", (row, col))
+                    elif row > size//2:
+                        case = p.Piece("B", (row, col))
+                    else:
+                        case = " "
                 else:
-                    case = " "
+                    case = "."
                 board[row].append(case)
 
         self._board = board
 
     def to_lines(self):
-        print(self._board)
-        for row in range(self._size):
+        for row in self._board:
             print("[ ", end="")
-            for case in self._board[row]:
-                if row % 2 == 0:
-                    print(".", end=" ")
-                if case != " ":
-                    print(case.get_color(), end=" ")
+            for case in row:
+                if type(case) is p.Piece:
+                    print(case._color, end=" ")
                 else:
                     print(case, end=" ")
-                if row % 2 != 0:
-                    print(".", end=" ")
             print("]")
         return
 
     def move(self, move):
+        if type(move) is c.Capture:
+            self._board[move._captured[0]][move._captured[1]] = " "
         pion = self._board[move._start[0]][move._start[1]]
         pion._pos = move._end
         self._board[move._end[0]][move._end[1]] = pion
