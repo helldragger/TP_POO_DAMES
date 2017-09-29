b721714ad3d62d0595a6130ab043a78d366755a9
BuildTools <unconfigured@null.spigotmc.org>
2017-09-29 13:22:49 +0200

    Added Piece subclasses (reine et pion) and atomic moves versions

--- Piece.py
+++ Piece.py
@@ -1,5 +1,5 @@
 import AtomicMove as am
-
+import Capture as cpt
 
 class Piece:
     def __init__(self, color, pos):
@@ -25,20 +25,90 @@ class Piece:
         go_up = self._color == "B"
         modif = 1 - 2 * int(go_up)
         moves = []
-        if  0 <= self._pos[0]+modif < len(board._board):
-            if board._board[self._pos[0]+modif][self._pos[1]] == " " :
-                moves.append(am.AtomicMove(self._pos, (self._pos[0]+modif, self._pos[1])))
-        if  0 <= self._pos[0]+modif < len(board._board) \
-                and 0 <= self._pos[1]+modif < len(board._board)//2:
-            if board._board[self._pos[0]+modif][self._pos[1] + modif] == " ":
-                moves.append(am.AtomicMove(self._pos, (self._pos[0]+modif, self._pos[1] + modif)))
+
+        x = self._pos[0]
+        y = self._pos[1]
+        if 0 <= x+modif < len(board._board):
+            if 0 <= y+modif < len(board._board):
+                if board._board[x+modif][y+modif] == " ":
+                    moves.append(am.AtomicMove(self._pos, (x+modif, y+modif)))
+            if 0 <= y-modif < len(board._board):
+                if board._board[x+modif][y-modif] == " ":
+                    moves.append(am.AtomicMove(self._pos, (x+modif, y-modif)))
         return moves
 
     def captures(self, board):
+        colors = ["B", "N"]
         go_up = self._color == "B"
         modif = 1 - 2 * int(go_up)
         moves = []
         x = self._pos[0]
         y = self._pos[1]
-        if -1 < x + modif *2 < len(board._board):
-            if 
+        if 0 <= x + modif * 2 < len(board._board):
+            if 0 <= y + 2 < len(board._board):
+                if board._board[x + modif * 2][y + 2] == " " \
+                    and type(board._board[x + modif][y + 1]) is Piece:
+                    if board._board[x + modif][y + 1]._color == colors[int(go_up)]:
+                        moves.append(cpt.Capture(self._pos,
+                                                 [x + modif, y + 1],
+                                                 [x + modif * 2, y + 2]))
+            if 0 <= y - 2 < len(board._board):
+                if board._board[x + modif * 2][y - 2] == " " \
+                    and type(board._board[x + modif][y - 1]) is Piece:
+                    if board._board[x + modif][y - 1]._color == colors[int(go_up)]:
+                        moves.append(cpt.Capture(self._pos,
+                                                 [x + modif, y - 1],
+                                                 [x + modif * 2, y - 2]))
+        return moves
+
+
+class Man(Piece):
+
+    def atomic_moves(self, board):
+        go_up = self._color == "B"
+        modif = 1 - 2 * int(go_up)
+        moves = []
+
+        x = self._pos[0]
+        y = self._pos[1]
+        if 0 <= x + modif < len(board._board):
+            if 0 <= y + modif < len(board._board):
+                if board._board[x + modif][y + modif] == " ":
+                    moves.append(am.AtomicMove(self._pos, (x + modif, y + modif)))
+            if 0 <= y - modif < len(board._board):
+                if board._board[x + modif][y - modif] == " ":
+                    moves.append(am.AtomicMove(self._pos, (x + modif, y - modif)))
+        return moves
+
+class Queen(Piece):
+    def atomic_moves(self, board):
+        moves = []
+        row = self._pos[0]
+        column = self._pos[1]
+
+        for rad in range(1, board._size):
+            if (column + rad < board._size and row + rad < board._size):
+                if type(board._board[row + rad][column + rad]) is Piece:
+                    break
+                else:
+                    moves.append(am.AtomicMove(self._pos, (row + rad, column + rad)))
+        for rad in range(1, board._size):
+            if (column + rad < board._size and row - rad >= 0):
+                if type(board._board[row - rad][column + rad]) is Piece:
+                    break
+                else:
+                    moves.append(am.AtomicMove(self._pos, (row + rad, column + rad)))
+        for rad in range(1, board._size):
+            if (column - rad >= 0 and row - rad >= 0):
+                if type(board._board[row - rad][column - rad]) is Piece:
+                    break
+                else:
+                    moves.append(am.AtomicMove(self._pos, (row + rad, column + rad)))
+        for rad in range(1, board._size):
+            if (column - rad >= 0 and row + rad < board._size):
+                if type(board._board[row + rad][column - rad]) is Piece:
+                    break
+                else:
+                    moves.append(am.AtomicMove(self._pos, (row + rad, column + rad)))
+        return moves
+
